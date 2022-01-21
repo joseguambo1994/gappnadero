2c6cebbd57ac755b6ca9146a3e3946858a35c911
jose <jlguambo@gmail.com>
2022-01-20 23:21:28 -0500

    Add List Sheep Screen

--- screens/TabFourScreen.tsx
+++ screens/TabFourScreen.tsx
@@ -1,17 +1,72 @@
-import { View, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
+import CowComponent, { CowProps } from '../components/CowComponent';
+import { collection, getDocs } from "firebase/firestore";
+import db from '../firebase';
+import { useEffect, useState } from 'react';
+import { RootTabScreenProps } from '../types';
+interface ISheep {
+  id: string,
+  name: string,
+  number: number,
+  birthday: string,
+  inseminada: boolean,
+  saludable: boolean,
+  ordenada: boolean,
+}
 
-export default function TabFourScreen() {
+const emptyArray: CowProps[] = [];
+
+export default function TabFourScreen({ navigation }: RootTabScreenProps<'TabTwo'>) {
+  const [arrayOfCows, setArrayOfCows] = useState(emptyArray);
+
+  async function getData(){
+    const querySnapshot = await getDocs(collection(db, "sheep"));
+    const tempArray: ISheep[] = [];
+    let tempItem: ISheep;
+    querySnapshot.forEach((doc) => {
+      
+      console.log(doc.id, " => ", doc.data());
+      tempItem= {
+        id: doc.id,
+        name: doc.get("name"),
+        number: Number(doc.get("number")),
+        birthday: doc.get("birthday").toDate().toDateString(),
+        inseminada: doc.get("inseminada"),
+        saludable: doc.get("saludable"),
+        ordenada: doc.get("ordenada"),
+      }
+      tempArray.push(tempItem);
+    });
+    setArrayOfCows(()=>tempArray);
+  }
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      console.log("Tab Two Focus")
+      getData();
+    });
+    return unsubscribe;
+  }, [navigation]);
+
   return (
-    <View style={styles.container}>
-    </View>
+    <ScrollView style={styles.container}>
+      {
+        arrayOfCows.map((item)=>(<CowComponent
+          name={item.name}
+          number={item.number}
+          birthday={item.birthday}
+          inseminada={item.inseminada}
+          saludable={item.saludable}
+          ordenada={item.ordenada}
+        ></CowComponent>))
+      }
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'red',
+    padding:5,
   },
 });
