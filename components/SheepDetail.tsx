7fd17fceedc2aa409cfaca7bc57f629f6fffd42b
jose <jlguambo@gmail.com>
2022-01-22 01:54:27 -0500

    Add image load from firestore

--- components/SheepDetail.tsx
+++ components/SheepDetail.tsx
@@ -1,8 +1,9 @@
-import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
+import { View, Text, StyleSheet, Image, ScrollView, TouchableOpacity } from "react-native";
 import { Dimensions } from "react-native";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, collection, getDocs } from "firebase/firestore";
 import db from "../firebase";
 import { useEffect, useState } from "react";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
 export type ISheepDetail = {
   name: string;
@@ -13,6 +14,11 @@ export type ISheepDetail = {
   birthdayDate: string;
   arrivalDate: string;
 };
+
+type ISheepWeight = {
+  date: string,
+  weight: number,
+}
 
 type SheepDetailProps = {
   animalId: string;
@@ -31,12 +37,15 @@ const SheepDetail = (props: SheepDetailProps) => {
   };
   const [animalDetail, setAnimalDetail] = useState(emptySheep);
 
+  const emptyArrayOfWeights: ISheepWeight[] = [];
+  const [arrayOfWeights, setArrayOfWeights] = useState(emptyArrayOfWeights);
+  const [showWeights, setShowWeights] = useState(false);
+
   async function getData() {
     const docSnapshot = await getDoc(doc(db, "sheep", props.animalId));
-    let tempItem: ISheepDetail;
     if (docSnapshot.exists()) {
       console.log("Document data:", docSnapshot.data());
-      const temporalSheep = {
+      const temporalSheep:ISheepDetail = {
         name: docSnapshot.data()["name"],
         number: docSnapshot.data()["number"],
         ill: docSnapshot.data()["ill"],
@@ -50,19 +59,55 @@ const SheepDetail = (props: SheepDetailProps) => {
       };
       setAnimalDetail(() => temporalSheep);
     } else {
-      // doc.data() will be undefined in this case
       console.log("No such document!");
     }
   }
 
+  async function getWeighing(){
+   // const citiesRef = (await getDocs(collection(db, "sheep",props.animalId))).docs;
+    const querySnapshot = await getDocs(collection(db, "sheep", props.animalId, "weighing"));
+    const tempArray: ISheepWeight[] = [];
+    let tempItem: ISheepWeight;
+    querySnapshot.forEach((doc) => {
+      
+      console.log(doc.id, " => ", doc.data());
+      tempItem= {
+        date: doc.get("date").toDate().toDateString(),
+        weight: Number(doc.get("weight")),
+      }
+      tempArray.push(tempItem);
+    });
+    setArrayOfWeights(()=>tempArray);
+  }
+
+  const storage = getStorage();
+  const [image, setImage] = useState('');
+
+useEffect(() => {
+  // let imageRef = firebase.storage().ref('/ankit/');
+  // imageRef
+  //   .getDownloadURL()
+  let imagePath = 'sheep/'+props.animalId+'/image.JPG'
+  const storage = getStorage();
+getDownloadURL(ref(storage, imagePath))
+    .then((url) => {
+       setImage(url);
+      console.log("URL1111111111111111: ",url)
+    })
+    .catch((e) => console.log('getting downloadURL of image error => ', e));
+},[props.animalId])
+
+
   useEffect(() => {
     getData();
+    getWeighing();
+    setShowWeights(()=>false);
   }, [props.animalId]);
 
   const renderRow = (label: string, text: string) => {
     return (
       <View style={styles.rowContainer}>
-        <Text>{label + " : "}</Text>
+        <Text>{label}</Text>
         <Text>{text}</Text>
       </View>
     );
@@ -71,19 +116,31 @@ const SheepDetail = (props: SheepDetailProps) => {
   return (
     <ScrollView>
       <View style={styles.container}>
-        <Image
+        {/* <Image
           style={styles.image}
           source={{
             uri: "https://s3-us-west-2.amazonaws.com/melingoimages/Images/86116.jpg",
           }}
+        /> */}
+        <Image
+          style={styles.image}
+          source={{
+            uri: image,
+          }}
         />
-        {renderRow("name", animalDetail.name)}
-        {renderRow("number", animalDetail.number.toString())}
-        {renderRow("ill", animalDetail.ill.toString())}
-        {renderRow("heat", animalDetail.heat.toString())}
-        {renderRow("pregnant", animalDetail.pregnant.toString())}
-        {renderRow("birthdayDate", animalDetail.birthdayDate.toString())}
-        {renderRow("arrivalDate", animalDetail.arrivalDate.toString())}
+        {renderRow("Número: ", animalDetail.number.toString())}
+        {renderRow("Nombre: ", animalDetail.name)}
+        {renderRow("Enferma: ", animalDetail.ill.toString())}
+        {renderRow("En celo: ", animalDetail.heat.toString())}
+        {renderRow("Preñada: ", animalDetail.pregnant.toString())}
+        {renderRow("Fecha de nacimiento: ", animalDetail.birthdayDate.toString())}
+        {renderRow("Fecha de llegada: ", animalDetail.arrivalDate.toString())}
+        <TouchableOpacity style={[styles.rowContainer, styles.buttonContainer]} onPress={()=> setShowWeights(!showWeights)}>
+          <Text style={styles.buttonText}>{'Pesajes'}</Text>
+        </TouchableOpacity>
+        {
+          showWeights === true ? arrayOfWeights.map(item => renderRow('Peso: ' +item.weight.toString() + 'Kg ', 'Fecha: '+ item.date)) : null
+        }
       </View>
     </ScrollView>
   );
@@ -92,7 +149,6 @@ const SheepDetail = (props: SheepDetailProps) => {
 const styles = StyleSheet.create({
   container: {
     padding:10,
-    backgroundColor: "yellow",
     alignItems: "center",
   },
   image: {
@@ -107,6 +163,13 @@ const styles = StyleSheet.create({
     borderColor: "blue",
     borderRadius: 10,
   },
+  buttonContainer: {
+    backgroundColor:'blue',
+  },
+  buttonText:{
+    color:'white',
+  }
+
 });
 
 export default SheepDetail;
